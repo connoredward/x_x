e6a7aed19d67f79eb6e1eea2c239df5a57cb97c6
connor <connor.chaplain@pipemind.com>
2020-08-06 11:17:58 +0100

    ch

--- api/auth.ts
+++ api/auth.ts
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 import config from '../config/var';
 
-const apiUrl = config.api.dev;
+const apiUrl = config.api.prod;
 
 export const checkToken = (): Promise<void | boolean> => {
   return fetch(apiUrl + 'checkToken', {
--- api/user.ts
+++ api/user.ts
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch';
 import config from '../config/var';
 
-const apiUrl = config.api.dev;
+const apiUrl = config.api.prod;
 
 type Props = {
   newUser: {
